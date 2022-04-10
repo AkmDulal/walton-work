f9e7f8b8c442c9ec8683ac5b9e44b6268496d3d0
Md Dulal Hossain <dulalhossain34220@gmail.com>
2022-04-10 10:53:51 +0600

    project upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
+
+import { useRef } from "react";
 import './App.css';
+import { Controller, Scene } from "react-scrollmagic";
+import SequenceOne from "./Components/SequenceOne";
+import SequenceTwo from "./Components/SequenceTwo";
+import SequenceThree from "./Components/SequenceThree";
+// import { Container, Row, Col } from "react-bootstrap";
+
+import LogoTop from "./headerimages/toplogo.png"
+import Logo from "./headerimages/TAMARIND.png"
+
+console.clear()
 
 function App() {
+  const ref = useRef();
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="hedaer__area">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-1">
+                <img src={LogoTop} alt="" />
+            </div>
+            <div className="col-lg-11">
+              <ul>
+                <li> Laptop </li>
+                <li> Desktop </li>
+                <li> All In One </li>
+                <li> Monitor </li>
+                <li> Keyboard </li>
+                <li> Mouse </li>
+                <li> Pendrive </li>
+                <li> Headphone </li>
+                <li> Accessories </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="hedaer__area_bottom">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-3">
+                <img src={Logo} alt="" />
+              </div>
+              <div className="col-lg-9">
+                <ul>
+                  <li> Overview </li>
+                  <li> Tech Specs </li>
+                  <li> Picture Gallery </li>
+                  <li> Buy Now </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="screen_one">
+        <Controller>
+          <Scene duration="200%" triggerHook="onLeave" pin>
+            {progress => (
+              <div style={{ height: "100vh", position: "relative" }}>
+                <SequenceOne ref={ref} progress={progress} />
+              </div>
+            )}
+          </Scene>
+        </Controller>
+      </div>
+      <div className="screen_one screen_two">
+        <Controller>
+          <Scene duration="200%" triggerHook="onLeave" pin>
+            {progress => (
+              <div style={{ height: "100vh", position: "relative" }}>
+                <SequenceTwo ref={ref} progress={progress} />
+              </div>
+            )}
+          </Scene>
+        </Controller>
+      </div>
+      <div className="screen_one screen_two">
+        <Controller>
+          <Scene duration="200%" triggerHook="onLeave" pin>
+            {progress => (
+              <div style={{ height: "100vh", position: "relative" }}>
+                <SequenceThree ref={ref} progress={progress} />
+              </div>
+            )}
+          </Scene>
+        </Controller>
+      </div>
+
     </div>
   );
 }
